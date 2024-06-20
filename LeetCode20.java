d73d39c135f4a99fc929818d13755aabfd5214c5
vamshi <vamshikrishnasagar77@gmail.com>
2024-06-20 19:17:39 +0530

    new commit

---
+++ LeetCode20.java
@@ -0,0 +1,43 @@
+import java.util.*;
+public class LeetCode20 {
+
+    public static void main(String[] args) {
+        String s = "(]";
+        boolean ans = isValid(s);
+        System.out.println(ans);
+        
+    }
+
+    public static boolean isValid(String s){
+
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : s.toCharArray()){
+            if(ch == '(' || ch == '{' || ch == '['){
+                stack.push(ch);
+            }else{
+                if(ch == ')'){
+                    if(stack.isEmpty() || stack.pop() != '('){
+                        return false;
+                    }
+                }
+                
+                if(ch == '}'){
+                    if(stack.isEmpty() || stack.pop() != '{'){
+                        return false;
+                    }
+                }
+
+                if(ch == ']'){
+                    if(stack.isEmpty() || stack.pop() != '['){
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return stack.isEmpty();
+
+    }
+    
+}
