96165a94ed0ad3eca16da58a408f41bcfe446564
vamshi <vamshikrishnasagar77@gmail.com>
2024-06-20 18:48:02 +0530

    new commit

---
+++ LeetCode9.java
@@ -0,0 +1,61 @@
+public class LeetCode9 {
+    
+    public static void main(String[] args) {
+        int num = 121;
+        boolean ans = isPalindrome(num);
+        System.out.println(ans);
+        
+    }
+
+    /*public static boolean isPalindrome(int x){
+
+        int temp = x;
+        int reverse = 0;
+
+        if(x==0){
+            return true;
+        }
+
+        if(x < 0 || x % 10 == 0){
+            return false;
+        }
+
+        while(x > 0){
+            int digit = x % 10;
+            reverse = reverse*10 + digit;
+            x = x / 10;
+        }
+
+        if(reverse == temp){
+            return true;
+        }else{
+            return false;
+        }
+    }*/
+
+    public static boolean isPalindrome(int x){
+
+        int temp =  x;
+        int reverse = 0;
+
+        if(x == 0){
+            return true;
+        }
+
+        if(x < 0 || x % 10 == 0){
+            return false;
+        }
+
+        while(x > 0){
+            int digit = x%10;
+            reverse = reverse*10+digit;
+            x = x/10;
+        }
+
+        if(reverse == temp){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
