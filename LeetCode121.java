4f532c9d027072cad4282691f3667d636c308f2c
vamshi <vamshikrishnasagar77@gmail.com>
2024-06-25 20:11:55 +0530

    new commit

---
+++ LeetCode121.java
@@ -0,0 +1,25 @@
+//121. Best Time to Buy and Sell Stock
+
+public class LeetCode121 {
+
+    public static void main(String[] args) {
+        int[] prices = {7,1,5,3,6,4};
+        int ans = buySellStock(prices);
+        System.out.println(ans);
+
+    }
+
+    public static int buySellStock(int[] prices){
+        int mini = Integer.MAX_VALUE;
+        int profit = 0;
+        int n = prices.length;
+
+        for(int i=0;i<n;i++){
+            int cost = prices[i] - mini;
+            profit = Math.max(profit,cost);
+            mini = Math.min(mini,prices[i]);
+        }
+        return profit;
+    }
+    
+}
